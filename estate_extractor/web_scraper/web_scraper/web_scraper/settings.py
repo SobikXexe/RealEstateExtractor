9abb9803c74cc747eeb98bde55f09ecdf7ce08ef
Daniel Šebík <sobikk.dan@gmail.com>
2022-12-02 12:56:38 +0100

    switched from splash to playwright, looping through properties on one site is ready

--- estate_extractor/web_scraper/web_scraper/web_scraper/settings.py
+++ estate_extractor/web_scraper/web_scraper/web_scraper/settings.py
@@ -12,22 +12,13 @@ BOT_NAME = 'web_scraper'
 SPIDER_MODULES = ['web_scraper.spiders']
 NEWSPIDER_MODULE = 'web_scraper.spiders'
 
-#splash config
-SPLASH_URL = 'http://localhost:8050'
-
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+# Playwright
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
 
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
-
-DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
-
-HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'web_scraper (+http://www.yourdomain.com)'
--- estate_extractor/web_scraper/web_scraper/web_scraper/spiders/sreality.py
+++ estate_extractor/web_scraper/web_scraper/web_scraper/spiders/sreality.py
@@ -1,37 +1,27 @@
 import scrapy
-from scrapy_splash import SplashRequest
-import requests
-
+from scrapy_playwright.page import PageMethod
 class SrealitySpider(scrapy.Spider):
     name = 'sreality'
-    base_url = 'https://www.sreality.cz/'
-    relative_address = 'hledani/prodej/byty'
-    start_urls = ['https://www.sreality.cz/']
+    _base_url = 'https://www.sreality.cz/'
+    _relative_address = 'hledani/prodej/byty'
+    properties_count = 0
 
-    lua_script = """
-        function main(splash)
-            splash:set_result_content_type("text/html; charset=utf-8")
-            assert(splash:go(splash.args.url))
-            assert(splash:set_content("<html><body><h1>hello</h1></body></html>")
-            return "AHA"
-        end
-        """
-
-    def __init__(self) -> None:
-        super().__init__()
 
     def start_requests(self):
 
-        return [SplashRequest(url=self.base_url + self.relative_address, callback=self.parse,
-                                endpoint="execute",
-                                args={"lua_source": self.lua_script},
-                                dont_filter=True)]
+        yield scrapy.Request(self._base_url + self._relative_address,
+                            callback=self.parse,
+                            meta={"playwright": True
+                                })
 
 
-    def parse(self, response):
-        print(response.text)
-        pass
+    async def parse(self, response):
+        title : str
+        img : str
+        for item in response.css('div.property.ng-scope'):
+            self.properties_count += 1
+            title = item.css('span.name.ng-binding::text').get()
+            print(title)
+        print(self.properties_count)
 
-    # def prepare_for_splash(self, html : str) -> str:
-    #     return html.replace('<head>', '<head><base href="https://www.sreality.cz/" /><script>window.location.assign("https://www.sreality.cz/hledani/prodej/byty")</script>')
         
